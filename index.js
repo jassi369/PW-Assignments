bdf649d82cbabe05a539e216a4d8478edf9c99bf
jassi369 <369jassi@gmail.com>
2023-08-11 22:36:53 +0530

    dont ask why

---
+++ index.js
@@ -0,0 +1,14 @@
+const num = [1, 2, 3, 4, 5];
+const name1 = ["jassi", "kirti", "smriti"];
+// num.push(2);
+// num.pop()
+// num.shift();
+// num.unshift('jassi');
+// let newArray = num.concat(name);
+//  num.splice(3,1,6);
+// name1.forEach(currentValue =>{
+//     console.log("hey "+ name1)
+// })
+
+let double = num.map(currentValue=>currentValue*3);
+console.log(double);
--- learn.js
+++
@@ -1,7 +0,0 @@
-const add = (a,b) => {
-    console.log(a*b);
-}
-add(3,3);
-function add(a,b){
-    console.log(a*b);
-}
