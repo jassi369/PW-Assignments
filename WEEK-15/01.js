ff4b355462b13cd46122647a4fd830a8054430a8
jassi369 <369jassi@gmail.com>
2023-08-24 04:07:39 +0530

    final update for the week 15 ASSIGNMENT

--- WEEK-15/01.js
+++ WEEK-15/01.js
@@ -1,12 +1,12 @@
 let car = {
-    make:"hundai",
-    model:"Grand i10",
-    year:2017
+  make: "hundai",
+  model: "Grand i10",
+  year: 2017,
 };
 
-function properties(){
-   for (const prop in car) {
+function properties() {
+  for (const prop in car) {
     console.log(`${prop} => ${car[prop]} `);
-   }
+  }
 }
 properties();
