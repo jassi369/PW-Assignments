5926782d02011c80a5be96cb418d5ce5fd3b444b
jassi369 <369jassi@gmail.com>
2023-09-06 23:51:00 +0530

    updates

--- WEEK-17-Promises-Callbacks/01.js
+++ WEEK-17-Promises-Callbacks/01.js
@@ -1,1 +1,12 @@
-console.log("Hello, Friend!");
+class car{
+    constructor(company,year,model,name,engine){
+        this.company = company;
+        this.year = year;
+        this.model = model;
+        this.name = name;
+        this.engine = engine;
+    }
+}
+
+const Bmw = new car("BMW",2022,"M5","BMW M5","V12")
+console.log(Bmw);
