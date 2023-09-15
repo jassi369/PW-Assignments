d8efc4c7c9146c08d0c9349f82d90489a3c2857d
jassi369 <369jassi@gmail.com>
2023-09-15 23:24:27 +0530

    updates

--- learn.js
+++
@@ -1,8 +0,0 @@
-const { createHash } = require('crypto');
-
-function hash(input){
-  return createHash('sha256').update(input).digest('base64');
-}
-let password = "Gill saab";
-const hash1 = hash(password);
-console.log(hash1);
